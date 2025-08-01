8a812fc88cb09aebc03949f804432606d0e88306
moong414 <moongom414@gmail.com>
2025-08-01 19:44:16 +0900

    fix: .env에 api키 등 정리

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_location_app/firebase_options.dart';
 import 'package:flutter_location_app/ui/home_page/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: MyApp()));
 }
